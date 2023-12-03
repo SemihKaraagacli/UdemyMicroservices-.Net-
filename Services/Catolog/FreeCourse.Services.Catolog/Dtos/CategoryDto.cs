51a52edcb6d9921a36f8d642fd1b6223b8afd1ef
SemihKaraagacli <karaagaclisemih@gmail.com>
2023-12-03 20:51:43 +0300

    UdemyMicroservice

---
+++ Services/Catolog/FreeCourse.Services.Catolog/Dtos/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Services.Catolog.Dtos
+{
+    public class CategoryDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } 
+    }
+}
---
+++ Services/Catolog/FreeCourse.Services.Catolog/Models/Feature.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeCourse.Services.Catolog.Models
+{
+    public class Feature
+    {
+        public int Duration { get; set; }
+    }
+}
